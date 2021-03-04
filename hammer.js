bd7259a7e774c1f51faa708dd6a23a5c4cfa14b8
NavdeepYadav <74094165+navdeep2008@users.noreply.github.com>
2021-03-04 14:04:24 +0530

    Add files via upload

--- hammer.js
+++ hammer.js
@@ -1,11 +1,11 @@
 class Hammer{
     constructor(x,y,width,height){
     var options = {
-    'density' : 2
-    'friction' : 1.0
-    'restitution' 0.5
+    'density' : 2,
+    'friction' : 1.0,
+    'restitution' :0.5
     };
-    this.body = Bodies.rectangle(x ,y,width,height,body_options);
+    this.body = Bodies.rectangle(x,y,width,height,options);
     this.width = width;
     this.height = height;
     World.add(world,this.body);
